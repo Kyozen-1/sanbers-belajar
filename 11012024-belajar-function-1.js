d101ff41fcc7b2a177757e71dc4eb05806dfe043
Kyozen-1 <skadi1268@gmail.com>
2024-01-11 20:39:51 +0700

    done funsi satu

--- 11012024-belajar-function-1.js
+++ 11012024-belajar-function-1.js
@@ -1,3 +1,18 @@
-console.log('======================================')
-console.log('')
-console.log('======================================')
+function luasPersegi(sisi) {
+    return sisi * sisi;
+}
+
+console.log(luasPersegi(4))
+console.log(luasPersegi(8))
+console.log(luasPersegi(9))
+
+// Buat function yang melakukan rumus luas PersegiPanjang 
+// dengan 2 parameter nilai yaitu sisi L = Panjang * Lebar
+
+function luasPersegiPanjang(panjang, lebar) {
+    return panjang * lebar
+}
+
+console.log(luasPersegiPanjang(2, 4))
+console.log(luasPersegiPanjang(3, 8))
+console.log(luasPersegiPanjang(5, 3))
